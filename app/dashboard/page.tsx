562f38ee35eb5d49c2bc37f496e573618a4c05c8
Mahmoud <mbathich@assentify.com>
2024-11-11 22:37:52 +0200

    try concepts

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>General dashboard</div>
+  )
+}
+
+export default dashboard;
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>________</div>
+  )
+}
+
+export default loading
